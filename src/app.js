7ff2b3af5878bd9132286ec473298755f53fc706
Rahul Wadekar <r.wadekar@wir-machen-druck.de>
2020-08-20 18:03:31 +0530

    heroku deployment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsDirectoryPath = path.join(__dirname, '../templates/views')
@@ -18,6 +19,6 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
 })
